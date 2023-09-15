66b905524c88fa85d380fa312b2a5c416ebf2a25
bananamensfear <138299181+bananamensfear@users.noreply.github.com>
2023-09-15 16:08:41 +0300

    a

---
+++ mafiaMM/Assets/Script/timer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class timer : MonoBehaviour
+{
+    [SerializeField] Image cooldown;
+    [SerializeField] bool coolingDown;
+    [SerializeField] float waitTime = 30.0f;
+    float timer1;
+    void Start()
+    {
+        float timer1 = waitTime;
+    }
+    void Update()
+    {
+        
+        cooldown.fillAmount -= 1.0f/timer1 * Time.deltaTime;
+        Debug.Log(timer1);
+        if (cooldown.fillAmount <= 0f)
+        {
+            timer1 = waitTime;
+            cooldown.fillAmount = 1.0f;
+        }
+    }
+}
