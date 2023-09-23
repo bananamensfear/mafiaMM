f1448a6dfe35ea66d081a4a4eddcaaf7a290fe72
Volvonik <volvo0unity@gmail.com>
2023-09-23 17:25:56 +0300

    Collect Timer

--- mafiaMM/Assets/Script/timer.cs
+++ mafiaMM/Assets/Script/timer.cs
@@ -10,61 +10,61 @@ public class timer : MonoBehaviour
     [SerializeField] bool coolingDown;
     [SerializeField] float waitTime = 30.0f;
     [SerializeField] Vector3 coinStartingPos;
-    [SerializeField] GameObject coin ;
+    [SerializeField] GameObject coin;
     [SerializeField] TMP_Text moneyDisplay;
     float money = 0;
     public float[] MoneyCollect;
     int coinLevel;
     float coinSpawnTimer;
-    [SerializeField] Image timerForWorker;
 
     [SerializeField] float collectCoinTimer;
     [SerializeField] float workerCollectDelay;
-    [SerializeField] bool workerCanCollectCoin ;
+    [SerializeField] bool workerCanCollectCoin = true;
+    [SerializeField] Image workerTimerObject;
 
     public void AddMoney(GameObject coin)
     {
-        if(workerCanCollectCoin)
+        if (workerCanCollectCoin)
         {
             Destroy(coin);
             money += MoneyCollect[coinLevel];
             moneyDisplay.text = "Money = " + money.ToString(".00");
             workerCanCollectCoin = false;
+            collectCoinTimer = workerCollectDelay;
         }
     }
     void Update()
     {
         CoinSpawnerTimer();
 
-        if(!workerCanCollectCoin)
+        if (!workerCanCollectCoin)
         {
             WorkerCollectDelay();
         }
-        
     }
 
     void CoinSpawnerTimer()
     {
-        workerCanCollectCoin = false;
         cooldown.fillAmount -= 1.0f / coinSpawnTimer * Time.deltaTime;
         if (cooldown.fillAmount <= 0f)
         {
             Instantiate(coin, transform.position, transform.rotation);
             coinSpawnTimer = waitTime;
             cooldown.fillAmount = 1.0f;
-            
+
         }
     }
 
     void WorkerCollectDelay()
     {
-        timerForWorker.fillAmount -= 1.0f / workerCollectDelay * Time.deltaTime;
-        if( timerForWorker.fillAmount <= 0)
+        collectCoinTimer -= Time.deltaTime;
+        workerTimerObject.fillAmount = collectCoinTimer / workerCollectDelay;
+
+        if (collectCoinTimer < 0)
         {
             workerCanCollectCoin = true;
-            timerForWorker.fillAmount = 1.0f;
-            
-
+            collectCoinTimer = workerCollectDelay;
+            workerTimerObject.fillAmount = workerCollectDelay;
         }
     }
 }
